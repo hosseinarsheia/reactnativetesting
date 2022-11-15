da02a314353d8ea07add24efb094a32f1103f9c9
Unknown <hosseinarsheia@yahoo.com>
2022-11-15 15:56:38 +0330

    stable
    - added prettier rules

---
+++ src/screens/FormikScreen/FormikScreen.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+
+const FormikScreen = props => {
+  return (
+    <View>
+      <Text>FormikScreen</Text>
+    </View>
+  );
+};
+
+export default FormikScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
