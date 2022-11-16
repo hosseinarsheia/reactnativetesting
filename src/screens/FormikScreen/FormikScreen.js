2a8631a008368b0146ede6044a0c786b60d52a61
Unknown <hosseinarsheia@yahoo.com>
2022-11-16 17:06:58 +0330

    stable

--- src/screens/FormikScreen/FormikScreen.js
+++ src/screens/FormikScreen/FormikScreen.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Button, TextInput, View, StyleSheet, Text } from 'react-native';
 import { Formik, useFormik } from 'formik';
-import { Icon } from '@rneui/themed';
+import { Icon, CheckBox } from '@rneui/themed';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+  email: yup.string().required(),
+  name: yup.string().required(),
+  isOn: yup.string().required(),
+  check: yup.boolean().oneOf([true], 'sss').required(),
+});
 
 const FormikScreen = props => {
   return (
     <Formik
+      validationSchema={validationSchema}
       validateOnBlur
-      initialValues={{ email: '', name: '', isOn: false }}
+      validateOnChange={false}
+      initialValues={{ email: '', name: '', isOn: false, check: false }}
       onSubmit={(values, helpers) => {
         console.log('values', values);
-        console.log('helpers', helpers);
-        helpers.resetForm();
+
+        // helpers.resetForm();
       }}>
-      {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors }) => (
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        setFieldValue,
+        errors,
+        touched,
+        setFieldTouched,
+      }) => (
         <View style={{ width: '90%' }}>
+          {console.log(' errors.check ', errors.check)}
           <TextInput
             onChangeText={handleChange('email')}
             onBlur={handleBlur('email')}
@@ -28,17 +48,28 @@ const FormikScreen = props => {
             value={values.name}
             style={styles.TextInput}
           />
+          <Text>{values.isOn ? 'On' : 'Off'}</Text>
+
+          <CheckBox
+            center
+            title="Click Here"
+            checked={values.check}
+            onPress={() => {
+              setFieldValue('check', !values.check);
+              setFieldTouched('check');
+            }}
+          />
+
+          <Text>{errors.check}</Text>
+
           <Button onPress={handleSubmit} title="Submit" />
 
-          <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
-
-          <Text>{values.isOn ? 'On' : 'Off'}</Text>
-          <Button
+          {/* <Button
             title="setFieldValue "
             onPress={() => {
               setFieldValue('isOn', !values.isOn);
             }}
-          />
+          /> */}
         </View>
       )}
     </Formik>
@@ -51,5 +82,9 @@ const styles = StyleSheet.create({
   TextInput: {
     borderWidth: 1,
     marginVertical: 20,
+  },
+
+  errorMessage: {
+    color: 'red',
   },
 });
